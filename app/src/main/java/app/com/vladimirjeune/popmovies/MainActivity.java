587ef3e7084b68cdce62eb7d02b1cb62946ec6bc
vladimirjeune <randyjeune@gmail.com>
2017-11-04 03:41:25 -0400

    feat: Obtain the API key from the file it is held in
    Since the API key cannot be held on a public repo (Github) you have placed
    it in a file that is ignored by .gitignore.  The file holding the API key
    string should be placed in the main/assets directory.  It should be the only
    thing in the file, with no other characters.  Once it is in place, it will
    be read and used in the project.

--- app/src/main/java/app/com/vladimirjeune/popmovies/MainActivity.java
+++ app/src/main/java/app/com/vladimirjeune/popmovies/MainActivity.java
@@ -1,13 +1,48 @@
 package app.com.vladimirjeune.popmovies;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String mTMDKey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        obtainTMDKey();
+
+    }
+
+    /**
+     * OBTAINTMDKEY - Reads the apiTMD.key file in the assets directory so that we can
+     * access: <a href="https://www.themoviedb.org/">https://www.themoviedb.org/</a> with
+     * the assigned key.  Keys can be acquired at the site.  Then string can be placed in
+     * a file.
+     */
+    private void obtainTMDKey() {
+        // In order for the movie requests to work we must obtain key from file in assets
+        try {
+
+            AssetManager assetManager = getAssets();
+
+            Scanner scanner = new Scanner(assetManager.open("apiTmd.key"));
+
+            mTMDKey = scanner.next();
+
+//            Toast.makeText(this, mTMDKey, Toast.LENGTH_LONG).show();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException io ) {
+            io.printStackTrace();
+        }
     }
 }
