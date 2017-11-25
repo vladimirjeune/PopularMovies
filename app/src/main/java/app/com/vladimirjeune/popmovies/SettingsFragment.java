20dcfc8f7339e6767dd1d091d136f05eb7694f1c
vladimirjeune <randyjeune@gmail.com>
2017-11-25 14:07:13 -0500

    feat:  Start setting up the Settings Menu

---
+++ app/src/main/java/app/com/vladimirjeune/popmovies/SettingsFragment.java
@@ -0,0 +1,10 @@
+package app.com.vladimirjeune.popmovies;
+
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+/**
+ * Created by vladimirjeune on 11/23/17.
+ */
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+}
