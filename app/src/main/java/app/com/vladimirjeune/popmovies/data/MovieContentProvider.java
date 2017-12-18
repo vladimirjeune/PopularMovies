ea0c41bf2aec06817baf852fbab442bef7619e93
vladimirjeune <randyjeune@gmail.com>
2017-12-18 04:17:18 -0500

    fix: Add some logging

--- app/src/main/java/app/com/vladimirjeune/popmovies/data/MovieContentProvider.java
+++ app/src/main/java/app/com/vladimirjeune/popmovies/data/MovieContentProvider.java
@@ -9,8 +9,11 @@ import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 import app.com.vladimirjeune.popmovies.data.MovieContract.MovieEntry;
+
+import static app.com.vladimirjeune.popmovies.MovieData.TAG;
 
 /**
  * ContentProvider for Movie Data
@@ -140,6 +143,7 @@ public class MovieContentProvider extends ContentProvider {
                         contentValues);
 
                 retUri = uri.buildUpon().appendPath("" + newId).build();
+                Log.d(TAG, "insert: Uri: " + retUri);
                 break;
             default:
                 throw new UnsupportedOperationException("Unknown Uri: " + uri);
